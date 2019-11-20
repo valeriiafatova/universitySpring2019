ef11e11e2c4d36a732ee769fe3a491687b76af07
Valeriia Fatova <valeriia.fatova@gmail.com>
2019-11-20 17:43:49 +0200

    03-02 add spring anotations

--- src/main/java/com/epam/university/contoller/LoginController.java
+++ src/main/java/com/epam/university/contoller/LoginController.java
@@ -2,33 +2,34 @@ package com.epam.university.contoller;
 
 import com.epam.university.entity.User;
 import com.epam.university.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
     @Resource
     private UserService userService;
 
     @PostMapping
-    public String login(HttpServletRequest request) {
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
-        System.out.println("login: " + login + ", password: " + password);
-
-        HttpSession session = request.getSession();
+    public String login(@RequestParam(name = "login") String login, 
+                        @RequestParam String password,
+                        HttpSession session) {
+        LOG.info("Try to login : {}, password : {} ", login, password);
 
         Optional<User> user = userService.validateUser(login, password);
         if(user.isPresent()){
             session.setAttribute("user", user);
-            return "index";
+            return "redirect:/";
         }
         
         session.setAttribute("error", "Error");
