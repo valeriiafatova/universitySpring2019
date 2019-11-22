bad3436c5cfec29bdf0127bc2a669564a3b1d31c
Valeriia Fatova <valeriia.fatova@gmail.com>
2019-11-22 17:54:16 +0200

    06-02 Add Basic Security

---
+++ src/main/java/com/epam/university/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.epam.university.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+                http
+                        .csrf().disable()
+                        .authorizeRequests()
+                        .antMatchers("/", "/index", "/css/**", "/js/**", "/img/**", "/fonts/**", "/scss/**", "/vendors/**")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                        .and()
+                        .httpBasic();
+        }
+}
