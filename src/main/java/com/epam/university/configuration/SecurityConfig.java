605f33637df3dfe0fbafc5107a99a71fe94048e1
Valeriia Fatova <valeriia.fatova@gmail.com>
2019-11-22 17:59:10 +0200

    06-03 Add In Memory User Details Manager

--- src/main/java/com/epam/university/configuration/SecurityConfig.java
+++ src/main/java/com/epam/university/configuration/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.epam.university.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .anyRequest().authenticated()
                         .and()
                         .httpBasic();
+        }
+
+        @Bean
+        @Override
+        public UserDetailsService userDetailsService() {
+                List<UserDetails> users = new ArrayList<>();
+                users.add(User.withDefaultPasswordEncoder().username("admin").password("nimda").roles("USER", "ADMIN").build());
+                users.add(User.withDefaultPasswordEncoder().username("user").password("nimda").roles("USER").build());
+                return new InMemoryUserDetailsManager(users);
         }
 }
